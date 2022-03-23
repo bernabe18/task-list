1135d618160d4d97ea9c29e561e4a86776d6dd3a
leandro <leanvallejos666@gmail.com>
2022-03-22 21:20:33 -0300

    agregamos mock de tareas y el interface de las misma

---
+++ src/app/mock-task.ts
@@ -0,0 +1,33 @@
+//importamos el archivo task para que el array reciba las task
+import { task } from "./task"
+
+//array de tipo task pra guardarmis task
+export const TASKS:task[]=[
+    {
+        id:1,
+        text:"terminando primer modulo de angular",
+        day:"Agosto 5 a las 12:00",
+        reminder:true
+    },
+    {
+        id:2,
+        text:"hacer las compras para la cena",
+        day:"Agosto 5 a las 17:00",
+        reminder:true
+    },
+    {
+        id:3,
+        text:"investigar sobre bootstrap",
+        day:"Agosto 5 a las 19:00",
+        reminder:true
+    }
+    , 
+    {
+        id:4,
+        text:"leer el libro favorito",
+        day:"Agosto 5 a las 21:00",
+        reminder:true
+    }
+]
+
+
