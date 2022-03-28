4769d3e99f4feef109f58bea309fd659e18d75e2
leandro <leanvallejos666@gmail.com>
2022-03-28 11:08:04 -0300

    agregadopPC

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import{task}from '../../task';
 //base de datos de tasks
 import{TASKS}from '../../mock-task'
-
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
@@ -12,7 +12,7 @@ import{TASKS}from '../../mock-task'
 export class TaskItemComponent implements OnInit {
 //atributo que recibe de tripo task que tienen la primer posicion del array de tasks
   @Input() task:task=TASKS[0];
-
+ faTimes=faTimes;
   constructor() { }
 
   ngOnInit(): void {
